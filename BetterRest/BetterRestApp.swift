1fd3ced08ce0e0daee74c7302f277f3c40b48e96
Dan Holtzman <danholtzman@apple.com>
2026-07-03 11:34:27 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Dan Holtzman on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
